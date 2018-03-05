2938a2edd88ffc2e8cef0266b54df97066bcab93
Butchy1596 <dterra0726@gmail.com>
2018-03-05 02:02:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 function add(a,b){
-  return a + b
+  return a + b;
 }
 
 function subtract(a,b){
-  return b - a
+  return b - a;
 }
+
+function multiply(a,b){
+  return a * b;
+}
+
+function divide(a,b){
+  return a / b;
+}
+
